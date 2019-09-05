1302cd4ba76669b0eb681a1f70fa01222850ab91
zbory <zborayb@gmail.com>
2019-09-05 13:35:41 +0200

    Add vector normalization function

--- balazs.cpp
+++ balazs.cpp
@@ -3,11 +3,28 @@
 #include <cmath>
 #include <fstream>
 
+// Normalize vector
+template<typename T>
+std::vector<T> normalize(std::vector<T> v){
+    std::vector<T> result(v);
+    T sum = 0;
+    for (int i = 0; i < v.size(); ++i) {
+        sum += v[i] * v[i];
+    }
+    double length = sqrt(sum);
+    for (int j = 0; j < result.size(); ++j) {
+        result[j] = result[j] / length;
+    }
+    return result;
+}
+
+// Calculate scale for projection matrix
 template<typename T>
 T scaleCoeff(T fovDeg){
     return 1/(tan(fovDeg * 0.5 * M_PI / 180));
 }
 
+// Generate projection matrix
 template<typename T>
 std::vector<std::vector<T>> projMx(T fovDeg, T clippingN, T clippingF){
     std::vector<std::vector<T>> mx;
@@ -25,6 +42,7 @@ std::vector<std::vector<T>> projMx(T fovDeg, T clippingN, T clippingF){
     return mx;
 }
 
+// Print 2D matrix
 template<typename T>
 void printMx(std::vector<std::vector<T>> mx)
 {
@@ -36,6 +54,7 @@ void printMx(std::vector<std::vector<T>> mx)
     }
 }
 
+// Print 1D matrix
 template<typename T>
 void printMx(std::vector<T> mx)
 {
@@ -45,6 +64,7 @@ void printMx(std::vector<T> mx)
     std::cout << '\n';
 }
 
+// Multiply vector with matrix
 template<typename T>
 std::vector<T> mxMult(std::vector<T> mx1, std::vector<std::vector<T>> mx2)
 {
@@ -65,6 +85,7 @@ std::vector<T> mxMult(std::vector<T> mx1, std::vector<std::vector<T>> mx2)
 
 int main()
 {
+    // Cube
     std::vector<std::vector<float>> points = {{0, 0, 0},
                                             {1, 0, 0},
                                             {1, 1, 0},
@@ -74,9 +95,13 @@ int main()
                                             {1, 1, 1},
                                             {0, 1, 1}};
 
+    // Make homogeneous coordinates
     for (int i = 0; i < points.size(); ++i) {
         points[i].push_back(1);
     }
+
+    std::vector<float> vv = {2,2,2};
+    printMx(normalize(vv));
 
 
     std::vector<std::vector<float>> projectionMx = projMx(60.0f, 0.1f, 50.f);
@@ -86,7 +111,7 @@ int main()
         projectedPoints.push_back(mxMult(points[j], projectionMx));
     }
 
-    printMx(projectedPoints);
+    //printMx(projectedPoints);
 
     return 0;
 }
